33b89be2bef38090ed0b8faac93d68cd532ab8c8
Alice Moyon <aliceamoyon@gmail.com>
2020-06-08 18:31:41 +0100

    Prettify text on Country Info screen
    - add padding, left align, and tab

--- lib/country_info_screen.dart
+++ lib/country_info_screen.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'country.dart';
 
 class CountryInfoScreen extends StatelessWidget {
-
   CountryInfoScreen(this.country);
 
   final Country country;
@@ -26,11 +26,36 @@ class CountryInfoScreen extends StatelessWidget {
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
-            Text(country.name ?? "name not available"),
-            Text(country.code),
-            Text("${country.count}"),
-            Text("${country.locations}"),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                'Country Name:\t\t\t\t\t\t\t${country.name ?? "not available"}',
+                style: TextStyle(fontSize: 18),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                'Country Code:\t\t\t\t\t\t\t\t${country.code}',
+                style: TextStyle(fontSize: 18),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                "Country Count:\t\t\t\t\t\t${country.count}",
+                style: TextStyle(fontSize: 18),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                "No. of Locations:\t\t${country.locations}",
+                style: TextStyle(fontSize: 18),
+              ),
+            ),
           ],
         ),
       ),
