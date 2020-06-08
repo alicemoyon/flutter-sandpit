8ffe0b9b5fec859fa4589736fc039cb9a204afe1
Alice Moyon <aliceamoyon@gmail.com>
2020-06-08 14:18:59 +0100

    Update AqScreen to get and display list of 100 countries

--- lib/aq_screen.dart
+++ lib/aq_screen.dart
@@ -2,54 +2,59 @@ import 'package:flutter/material.dart';
 
 import 'networking.dart';
 
-var url = 'https://api.openaq.org/v1/countries?limit=1';
+var url = 'https://api.openaq.org/v1/countries?limit=100';
 
 class AqScreen extends StatelessWidget {
-
-  static Future<String> getCountries() async {
+  static Future<List> getCountries() async {
     NetworkHelper nh = NetworkHelper(url);
 
     var countryData = await nh.getData();
-    String countryName = countryData['results'][0]['name'];
-    return countryName;
+    List countryInfo = countryData['results'];
+    return countryInfo;
   }
 
-  final Future<String> countryName = getCountries();
+  final Future<List> countryInfo = getCountries();
 
   @override
   Widget build(BuildContext context) {
     return FutureBuilder(
-      future: countryName,
-      builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
-        if (snapshot.hasError)
-          return Text("Error: ${snapshot.error.toString()}");
-        switch (snapshot.connectionState) {
-          case ConnectionState.none:
-            return Text("No connection");
-          case ConnectionState.waiting:
-            return Center(
-              child: Column(
-                children: <Widget>[
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text("fetching the flavour cards"),
-                  ),
-                  CircularProgressIndicator(),
-                ],
-                mainAxisAlignment: MainAxisAlignment.center,
-              ),
-            );
-          case ConnectionState.active:
-          case ConnectionState.done:
-            return ListView(
-              children: <Widget>[
-                Text(snapshot.data),
-              ],
-            );
-          default:
-            return Text("something else happened",);
-        }
-      }
-    );
+        future: countryInfo,
+        builder: (BuildContext context, AsyncSnapshot<List> snapshot) {
+          if (snapshot.hasError)
+            return Text("Error: ${snapshot.error.toString()}");
+          switch (snapshot.connectionState) {
+            case ConnectionState.none:
+              return Text("No connection");
+            case ConnectionState.waiting:
+              return Center(
+                child: Column(
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text("fetching the flavour cards"),
+                    ),
+                    CircularProgressIndicator(),
+                  ],
+                  mainAxisAlignment: MainAxisAlignment.center,
+                ),
+              );
+            case ConnectionState.active:
+            case ConnectionState.done:
+              return ListView.builder(
+                itemCount: snapshot.data.length,
+                itemBuilder: (BuildContext context, int index) {
+                  if (snapshot.data[index]['name'] != null) {
+                    return Text(snapshot.data[index]['name']);
+                  } else {
+                    return Text(snapshot.data[index]['code']);
+                  }
+                },
+              );
+            default:
+              return Text(
+                "something else happened",
+              );
+          }
+        });
   }
 }
